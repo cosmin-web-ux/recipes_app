4cef5121e89a9cd9ea22250611fa1f824752f09b
cosmin-web-ux <rising.force@yahoo.com>
2020-03-23 00:13:13 +0200

    Add functionality to use Output and EventEmitter

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+  collapsed = false;
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
+}
