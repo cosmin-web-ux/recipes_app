87d1d5952e1c197be9e14f35bb50948f2346de04
cosmin-web-ux <rising.force@yahoo.com>
2020-03-29 21:38:14 +0300

    use_services_dependency_injection

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,39 @@
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
-
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+@Injectable()
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://previews.123rf.com/images/peteers/peteers1608/peteers160800243/61209413-wiener-schnitzel-with-french-fries-salad-and-a-sharp-dip.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Another Recipe',
+      'This is simply a test',
+      'https://www.fatburgercanada.com/wp-content/uploads/2015/07/fatburger_0014_UltimateBBQ.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    ),
   ];
+
+  constructor(private slService: ShoppingListService) {};
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
