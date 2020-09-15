5c98c7fccb688686a465b375aa5d8007d75c9803
cosmin-web-ux <rising.force@yahoo.com>
2020-09-15 23:14:28 +0300

    add routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,9 +6,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = false;
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 }
