914ae0a3e2f8e8a44da7da9a6be999882399fc01
cosmin-web-ux <rising.force@yahoo.com>
2020-09-20 19:29:05 +0300

    Add Firebase API functionality, data-storage service and resolver service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,29 +9,35 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 })
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [];
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://previews.123rf.com/images/peteers/peteers1608/peteers160800243/61209413-wiener-schnitzel-with-french-fries-salad-and-a-sharp-dip.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]
-    ),
-    new Recipe(
-      'Another Recipe',
-      'This is simply a test',
-      'https://www.fatburgercanada.com/wp-content/uploads/2015/07/fatburger_0014_UltimateBBQ.png',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A Test Recipe',
+  //     'This is simply a test',
+  //     'https://previews.123rf.com/images/peteers/peteers1608/peteers160800243/61209413-wiener-schnitzel-with-french-fries-salad-and-a-sharp-dip.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Another Recipe',
+  //     'This is simply a test',
+  //     'https://www.fatburgercanada.com/wp-content/uploads/2015/07/fatburger_0014_UltimateBBQ.png',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1)
+  //     ]
+  //   ),
+  // ];
 
-  constructor(private slService: ShoppingListService) {};
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
